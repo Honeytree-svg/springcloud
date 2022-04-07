38dfef6842ed81d3a9c36f95a5ce8a9fa0474ec2
Honeytree-svg <1370376044@qq.com>
2022-04-07 21:35:48 +0800

    sz

---
+++ cloudproviderpayment8001/src/main/java/com/sz/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.sz.springcloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.sz.springcloud.dao")
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
