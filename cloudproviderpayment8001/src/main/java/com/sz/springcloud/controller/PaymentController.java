bea69adb21dc799227941aea0fc361a03c7a8cd0
Honeytree-svg <1370376044@qq.com>
2022-04-09 00:50:49 +0800

    sz

--- cloudproviderpayment8001/src/main/java/com/sz/springcloud/controller/PaymentController.java
+++ cloudproviderpayment8001/src/main/java/com/sz/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.sz.springcloud.entity.CommonResult;
 import com.sz.springcloud.entity.Payment;
 import com.sz.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,6 +16,9 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
 
@@ -22,7 +26,7 @@ public class PaymentController {
         log.info("******插入结果:" + result);
         if (result >0){
 
-            return new CommonResult<>(200,"插入数据库成功",result);
+            return new CommonResult<>(200,"插入数据库成功,serverPort:" + serverPort,result);
         }else {
             return new CommonResult<>(400,"插入数据库失败",null);
         }
@@ -35,7 +39,7 @@ public class PaymentController {
         log.info("******插入结果:" + payment);
         if (payment !=null){
 
-            return new CommonResult<>(200,"查询成功",payment);
+            return new CommonResult<>(200,"查询成功,serverPort:"+serverPort,payment);
         }else {
             return new CommonResult<>(400,"查询失败",null);
         }
